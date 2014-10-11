c4111e20813ff797e6afdb4ff9d4e9f115e00c63
Oskar Gewalli <oskar@gewalli.se>
2014-10-11 08:33:01 +0200

    Vagrant setup with redis

---
+++ vagrant/provisioning/dev-setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+source /usr/local/rvm/scripts/rvm
+
+cd /vagrant/
+
+echo cd \/vagrant > /home/vagrant/.bashrc
---
+++ vagrant/provisioning/install-rvm.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+apt-get install -y curl
+curl -sSL https://get.rvm.io | bash -s $1
